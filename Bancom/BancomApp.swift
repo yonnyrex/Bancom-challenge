e8099ce7c1fad6c2cd4392a08c03378f68fc58a3
yonnyrex <92613578+yonnyrex@users.noreply.github.com>
2023-11-07 10:04:31 -0500

    Initial Commit

---
+++ Bancom/BancomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BancomApp.swift
+//  Bancom
+//
+//  Created by Yonny on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BancomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
